7e7c24173906fa302e7900d1857150fbafb4df43
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-03-31 18:38:19 +0100

    Added footer with links to social handle, GitHub repository and Netlify hosted pagepage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="Dictionary App">
+      <div className="container">
+        <footer className="text-align">
+          This project was coded by{" "}
+          <a
+            href="https://www.instagram.com/kimberleythornton_/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kimberley Thornton
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kimberleyjthornton-debug/dictionary-project-kjt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://dictionary-app-kjt.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
