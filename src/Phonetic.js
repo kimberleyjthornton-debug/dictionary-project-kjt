1d317d41a493a9c531875127e3b9a5c3a567582a
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-04-01 20:27:13 +0100

    Amended code so that the phonetic text only appears if it exists and the option for the audio only appears if the URL exists.

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <p>
-        {props.phonetic.text}{" "}
+      {props.phonetic.text && (
+        <span className="text">{props.phonetic.text}</span>
+      )}
+
+      {props.phonetic.audio && (
         <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-          <br /> Listen{" "}
+          Listen
         </a>
-      </p>
+      )}
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
+        {props.results.phonetics.slice(0, 1).map(function (phonetic, index) {
           return (
             <div key={index}>
               <Phonetic phonetic={phonetic} />
