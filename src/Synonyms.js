e0ba65b8be1fb5c2d7570f7e27c46aed75c65c27
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-04-01 19:30:32 +0100

    Sliced the props.meaning call to only display one definition, added synonums component to display synonyms if appearing in a listed format

---
+++ src/Synonyms.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms && props.synonyms.length > 0) {
+    return (
+      <div className="Synonyms-container">
+        <strong>Synonyms: </strong>
+        <ul className="Synonyms">
+          {props.synonyms.slice(0, 4).map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
